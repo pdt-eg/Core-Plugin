658e5698a756fcb9e45549fc113e77c5659a2555
KUBO Atsuhiro <kubo@iteman.jp>
2013-04-23 17:23:13 +0900

    changed the PHPConventions class so that identifier validation works properly. (Issue #48)

--- org.pdtextensions.core/src/org/pdtextensions/core/PHPConventions.java
+++ org.pdtextensions.core/src/org/pdtextensions/core/PHPConventions.java
@@ -7,23 +7,21 @@
  ******************************************************************************/
 package org.pdtextensions.core;
 
-import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.dltk.core.IModelStatus;
-import org.eclipse.php.internal.core.documentModel.parser.PHPTokenizer;
-import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
 
 /**
  * @since 0.17.0
  */
-@SuppressWarnings("restriction")
 public final class PHPConventions {
-	private static final PHPTokenizer SCANNER = new PHPTokenizer();
+    private static final Pattern PATTERN_FIELD_NAME = Pattern.compile("^\\$[a-zA-Z_\\x7f-\\xff\\u0100-\\uffff][a-zA-Z0-9_\\x7f-\\xff\\u0100-\\uffff]*$"); //$NON-NLS-1$
+    private static final Pattern PATTERN_LABEL = Pattern.compile("^[a-zA-Z_\\x7f-\\xff\\u0100-\\uffff][a-zA-Z0-9_\\x7f-\\xff\\u0100-\\uffff]*$"); //$NON-NLS-1$
 
 	public static IStatus validateFieldName(String name) {
-		return validateIdentifier(name);
+		return validateIdentifier(name, PATTERN_FIELD_NAME);
 	}
 
 	public static IStatus validateMethodName(String name) {
@@ -34,26 +32,15 @@ public final class PHPConventions {
 		return validateIdentifier(name);
 	}
 
-	public static IStatus validateIdentifier(String id) {
-		if (scannedIdentifier(id) != null) {
+	public static IStatus validateIdentifier(String id, Pattern pattern) {
+		if (pattern.matcher(id).matches()) {
 			return IModelStatus.VERIFIED_OK;
 		} else {
 			return new Status(IStatus.ERROR, PEXCorePlugin.PLUGIN_ID, -1, Messages.bind(Messages.convention_illegalIdentifier, id), null);
 		}
 	}
 
-	private static synchronized char[] scannedIdentifier(String id) {
-		if (id == null) return null;
-
-		ITextRegion token;
-		SCANNER.reset(id.toCharArray());
-
-		try {
-			token = SCANNER.getNextToken();
-		} catch (IOException e) {
-			return null;
-		}
-
-		return token.toString().toCharArray();
+	public static IStatus validateIdentifier(String id) {
+		return validateIdentifier(id, PATTERN_LABEL);
 	}
 }
