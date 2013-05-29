08ebb5df01255facd0e7a8f9f9465bdeda988910
KUBO Atsuhiro <kubo@iteman.jp>
2013-05-29 12:34:06 +0900

    changed the RenameTypeProcessor#updateReferences() to update type references with namespaces properly. (Issue #48)

--- org.pdtextensions.core.ui/src/org/pdtextensions/internal/corext/refactoring/rename/RenameTypeProcessor.java
+++ org.pdtextensions.core.ui/src/org/pdtextensions/internal/corext/refactoring/rename/RenameTypeProcessor.java
@@ -15,26 +15,34 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.core.DLTKContentTypeManager;
-import org.eclipse.dltk.core.IExternalSourceModule;
 import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.SourceParserUtil;
 import org.eclipse.dltk.core.search.IDLTKSearchConstants;
 import org.eclipse.dltk.core.search.SearchEngine;
 import org.eclipse.dltk.core.search.SearchMatch;
 import org.eclipse.dltk.core.search.SearchParticipant;
 import org.eclipse.dltk.core.search.SearchPattern;
 import org.eclipse.dltk.core.search.SearchRequestor;
+import org.eclipse.dltk.core.search.TypeReferenceMatch;
+import org.eclipse.dltk.internal.corext.refactoring.RefactoringAvailabilityTester;
 import org.eclipse.dltk.internal.corext.refactoring.changes.DynamicValidationRefactoringChange;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.resource.RenameResourceChange;
 import org.eclipse.php.internal.core.PHPLanguageToolkit;
+import org.eclipse.php.internal.core.compiler.ast.nodes.FullyQualifiedReference;
+import org.eclipse.php.internal.core.compiler.ast.visitor.PHPASTVisitor;
 import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.ReplaceEdit;
+import org.pdtextensions.core.ui.PEXUIPlugin;
 import org.pdtextensions.core.ui.refactoring.IPHPRefactorings;
 import org.pdtextensions.core.ui.refactoring.IRefactoringProcessorIds;
 import org.pdtextensions.internal.corext.refactoring.Checks;
@@ -117,21 +125,47 @@ public class RenameTypeProcessor extends PHPRenameProcessor {
 			SearchPattern.createPattern(
 				modelElement,
 				IDLTKSearchConstants.REFERENCES,
-				SearchPattern.R_EXACT_MATCH | SearchPattern.R_ERASURE_MATCH,
+				SearchPattern.R_FULL_MATCH,
 				PHPLanguageToolkit.getDefault()
 			),
 			new SearchParticipant[]{ SearchEngine.getDefaultSearchParticipant() },
 			SearchEngine.createWorkspaceScope(PHPLanguageToolkit.getDefault()),
 			new SearchRequestor() {
 				@Override
-				public void acceptSearchMatch(SearchMatch match) throws CoreException {
-					if (match.getElement() instanceof IModelElement) {
-						ISourceModule module = (ISourceModule) ((IModelElement) match.getElement()).getAncestor(IModelElement.SOURCE_MODULE);
-						if (!(module instanceof IExternalSourceModule)) {
-							try {
-								addTextEdit(changeManager.get(module), getProcessorName(), new ReplaceEdit(match.getOffset(), currentName.length(), getNewElementName()));
-							} catch (MalformedTreeException e) {
-								// conflicting update -> omit text match
+				public void acceptSearchMatch(final SearchMatch match) throws CoreException {
+					if (match instanceof TypeReferenceMatch && match.getElement() instanceof IModelElement) {
+						final ISourceModule module = (ISourceModule) ((IModelElement) match.getElement()).getAncestor(IModelElement.SOURCE_MODULE);
+						if (module != null && RefactoringAvailabilityTester.isRenameAvailable(module)) {
+							ModuleDeclaration moduleDeclaration = SourceParserUtil.getModuleDeclaration(module);
+							if (moduleDeclaration != null) {
+								try {
+									moduleDeclaration.traverse(new PHPASTVisitor() {
+										@Override
+										public boolean visit(FullyQualifiedReference s) throws Exception {
+											if (s.sourceStart() == match.getOffset() && s.sourceEnd() == match.getOffset() + match.getLength()) {
+												try {
+													addTextEdit(
+														changeManager.get(module),
+														getProcessorName(),
+														new ReplaceEdit(
+															s.getNamespace() == null ? match.getOffset() : s.getNamespace().sourceEnd() + 1,
+															currentName.length(),
+															getNewElementName()
+														)
+													);
+												} catch (MalformedTreeException e) {
+													// conflicting update -> omit text match
+												}
+
+												return false;
+											}
+
+											return true;
+										}
+									});
+								} catch (Exception e) {
+									throw new CoreException(new Status(IStatus.ERROR, PEXUIPlugin.PLUGIN_ID, e.getMessage(), e));
+								}
 							}
 						}
 					}
