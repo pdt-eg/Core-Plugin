4b423020d6b8d2f987a62cbd468f56843b25d46a
Dawid Pakuła <zulus@w3des.net>
2015-08-28 18:01:48 +0200

    Improve formatting during code generation
    Run as DocumentChange and register with undo manager.
    
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- org.pdtextensions.core.ui/src/org/pdtextensions/core/ui/actions/GenerateGettersHandler.java
+++ org.pdtextensions.core.ui/src/org/pdtextensions/core/ui/actions/GenerateGettersHandler.java
@@ -62,7 +62,6 @@ import org.pdtextensions.core.ui.dialog.GetterSetterDialog;
 import org.pdtextensions.core.ui.util.GetterSetterUtil;
 import org.pdtextensions.core.util.PDTFormatterUtils;
 
-
 /**
  * 
  * Generates getters for private fields.
@@ -72,24 +71,23 @@ import org.pdtextensions.core.util.PDTFormatterUtils;
  */
 @SuppressWarnings("restriction")
 public class GenerateGettersHandler extends SelectionHandler implements IHandler {
-	
+
 	private IEditorPart editorPart;
 	private PHPStructuredEditor textEditor;
 	private IDocument document;
-	
-    private Map options;
+
+	private Map options;
 	private SourceType type;
 	private String lineDelim;
 	private boolean insertFirst = false;
 	private IModelElement insertAfter = null;
-	
-	
+
 	@SuppressWarnings("rawtypes")
 	private static class GetterSetterContentProvider implements ITreeContentProvider {
 
 		private Map fields;
 		private static final Object[] EMPTY = new Object[0];
-		
+
 		public GetterSetterContentProvider(Map fields) {
 
 			this.fields = fields;
@@ -98,35 +96,34 @@ public class GenerateGettersHandler extends SelectionHandler implements IHandler
 		@Override
 		public void dispose() {
 			fields.clear();
-			fields = null;			
+			fields = null;
 		}
 
 		@Override
 		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-						
+
 		}
 
 		@Override
 		public Object[] getElements(Object inputElement) {
 
 			return fields.keySet().toArray();
-			
 
 		}
 
 		@Override
 		public Object[] getChildren(Object parentElement) {
-			
-			if (parentElement instanceof IField) {
-			    
-			    Object[] elems = (Object[]) fields.get(parentElement);
 
-			    for (Object elem : elems) {
-			        GetterSetterEntry entry = (GetterSetterEntry) elem;
-			    }
+			if (parentElement instanceof IField) {
+
+				Object[] elems = (Object[]) fields.get(parentElement);
+
+				for (Object elem : elems) {
+					GetterSetterEntry entry = (GetterSetterEntry) elem;
+				}
 				return elems;
 			}
-			
+
 			return EMPTY;
 		}
 
@@ -138,273 +135,263 @@ public class GenerateGettersHandler extends SelectionHandler implements IHandler
 			}
 			if (element instanceof GetterSetterEntry)
 				return ((GetterSetterEntry) element).field;
-			
+
 			return null;
 		}
 
 		@Override
 		public boolean hasChildren(Object element) {
- 
+
 			return getChildren(element).length > 0;
 
 		}
 	}
-	
+
 	private class GetterSetterLabelProvider extends LabelProvider {
 
-		
 		@Override
 		public Image getImage(Object element) {
-			
-			if (element instanceof GetterSetterEntry) {				
+
+			if (element instanceof GetterSetterEntry) {
 				return DLTKUIPlugin.getImageDescriptorRegistry().get(DLTKPluginImages.DESC_FIELD_PUBLIC);
-			} else if (element instanceof IField){
+			} else if (element instanceof IField) {
 				return DLTKUIPlugin.getImageDescriptorRegistry().get(DLTKPluginImages.DESC_FIELD_PRIVATE);
-				
+
 			}
 			return super.getImage(element);
 
 		}
+
 		@Override
 		public String getText(Object element) {
 
 			if (element instanceof GetterSetterEntry) {
 				GetterSetterEntry entry = (GetterSetterEntry) element;
 				return entry.getName();
-			} else if (element instanceof IField){
-				return GetterSetterUtil.getFieldName(((IField)element));
-				
+			} else if (element instanceof IField) {
+				return GetterSetterUtil.getFieldName(((IField) element));
+
 			}
-			
+
 			return super.getText(element);
 		}
 	}
-	
-	protected Object exitMessage()
-	{
-	    final Shell p = DLTKUIPlugin.getActiveWorkbenchShell();
-	    MessageDialog.openInformation(p, "Getter/Setter generation unavailable", "No fields to generate getters/setters found.");
-	    return null;
+
+	protected Object exitMessage() {
+		final Shell p = DLTKUIPlugin.getActiveWorkbenchShell();
+		MessageDialog.openInformation(p, "Getter/Setter generation unavailable",
+				"No fields to generate getters/setters found.");
+		return null;
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		IModelElement element = getCurrentModelElement(event);
-		
-		if (element == null) {			
-		    return exitMessage();
+
+		if (element == null) {
+			return exitMessage();
 		}
-		
-		if (!(element instanceof SourceType)) {			
-			while(element.getParent() != null) {				
-				element = element.getParent();				
+
+		if (!(element instanceof SourceType)) {
+			while (element.getParent() != null) {
+				element = element.getParent();
 				if (element instanceof SourceType) {
 					break;
 				}
 			}
 		}
-		
+
 		if (element == null || !(element instanceof SourceType)) {
 			return exitMessage();
 		}
-		
+
 		type = (SourceType) element;
-		
+
 		try {
 
 			if (type.getFields().length == 0) {
-			    return exitMessage();
+				return exitMessage();
 			}
 
 			initialize(event, element);
-			
-			Map fields = getFields();			
+
+			Map fields = getFields();
 			final Shell p = DLTKUIPlugin.getActiveWorkbenchShell();
 			GetterSetterContentProvider cp = new GetterSetterContentProvider(fields);
 			GetterSetterDialog dialog = new GetterSetterDialog(p, new GetterSetterLabelProvider(), cp, type);
-			
+
 			dialog.setContainerMode(true);
 			dialog.setInput(type);
 			dialog.setTitle("Generate Getters and Setters");
-			
+
 			if (fields.size() == 0) {
-			    return exitMessage();
+				return exitMessage();
 			}
-			
+
 			if (dialog.open() == Window.OK) {
-				
-				List<GetterSetterEntry> entries = new ArrayList<GetterSetterEntry>();				
+
+				List<GetterSetterEntry> entries = new ArrayList<GetterSetterEntry>();
 				Object[] dialogResult = dialog.getResult();
-				
-				for (Object o : dialogResult) {					
-					if (o instanceof GetterSetterEntry) {				
-						entries.add((GetterSetterEntry) o);						
+
+				for (Object o : dialogResult) {
+					if (o instanceof GetterSetterEntry) {
+						entries.add((GetterSetterEntry) o);
 					}
 				}
-				
+
 				insertFirst = false;
 				insertAfter = null;
-				
+
 				if (dialog.insertAsFirstMember())
 					insertFirst = true;
 				else if (dialog.insertAsLastMember()) {
-				} else insertAfter = dialog.getInsertionPoint();
-				
+				} else
+					insertAfter = dialog.getInsertionPoint();
+
 				generate(entries, dialog.getModifier(), dialog.doGenerateComments(), dialog.hasFluentInterface());
-				
+
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return null;
 	}
-	
-	
-	private void generate(final List<GetterSetterEntry> entries, final int modifier, final boolean generateComments, final boolean fluent) throws Exception {
 
-		ISourceModule source = type.getSourceModule();			
-		String name = type.getElementName().replace("$", "");			
-		StringBuffer buffer = new StringBuffer(name);			
-		buffer.replace(0, 1, Character.toString(Character.toUpperCase(name.charAt(0))));			
-		name = buffer.toString();			
-					
+	private void generate(final List<GetterSetterEntry> entries, final int modifier, final boolean generateComments,
+			final boolean fluent) throws Exception {
+
+		ISourceModule source = type.getSourceModule();
+		String name = type.getElementName().replace("$", "");
+		StringBuffer buffer = new StringBuffer(name);
+		buffer.replace(0, 1, Character.toString(Character.toUpperCase(name.charAt(0))));
+		name = buffer.toString();
+
 		ASTParser parser = ASTParser.newParser(source);
 		parser.setSource(document.get().toCharArray());
-		
-		Program program = parser.createAST(new NullProgressMonitor());			
-//		program.recordModifications();		
-//		AST ast = program.getAST();		
 
-		ISourceRange range = type.getSourceRange();		
+		Program program = parser.createAST(new NullProgressMonitor());
+		// program.recordModifications();
+		// AST ast = program.getAST();
+
+		ISourceRange range = type.getSourceRange();
 		ASTNode node = program.getElementAt(range.getOffset());
 
 		if (!(node instanceof ClassDeclaration)) {
-			return ;				
+			return;
 		}
-		
-		char indentChar = FormatterUtils
-				.getFormatterCommonPrferences().getIndentationChar(document);
+
+		char indentChar = FormatterUtils.getFormatterCommonPrferences().getIndentationChar(document);
 		String indent = String.valueOf(indentChar);
-		
+
 		ClassDeclaration clazz = (ClassDeclaration) node;
 		Block body = clazz.getBody();
 		List<Statement> bodyStatements = body.statements();
-				
 
-		int end = bodyStatements.get(bodyStatements.size()-1).getEnd();
-		
+		int end = bodyStatements.get(bodyStatements.size() - 1).getEnd();
+
 		if (insertFirst) {
 			end = bodyStatements.get(0).getStart() - 1;
 		} else if (insertAfter != null) {
-			
+
 			boolean found = false;
-			
-			for (IMethod method : type.getMethods()) {				
+
+			for (IMethod method : type.getMethods()) {
 				if (method == insertAfter) {
 					ISourceRange r = method.getSourceRange();
 					end = r.getOffset() + r.getLength();
 					found = true;
 				}
 			}
-			
-			if (!found) {				
+
+			if (!found) {
 				for (IField field : type.getFields()) {
 					ISourceRange r = field.getSourceRange();
 					end = r.getOffset() + r.getLength() + 1;
 				}
 			}
 		}
-		
+
 		lineDelim = TextUtilities.getDefaultLineDelimiter(document);
 		String methods = "";
-		
+
 		int i = 0;
 		for (GetterSetterEntry entry : entries) {
-			
+
 			String code = "";
-			
+
 			if (!entry.isGetter) {
-				code = GetterSetterUtil.getSetterStub(entry.field, 
-						entry.getIdentifier(), entry.getType(), generateComments, modifier, indent, fluent);
-				
+				code = GetterSetterUtil.getSetterStub(entry.field, entry.getIdentifier(), entry.getType(),
+						generateComments, modifier, indent, fluent);
+
 			} else {
-				code = GetterSetterUtil.getGetterStub(entry.field, 
-						entry.getIdentifier(), generateComments, modifier, indent);				
-			}		
-			
+				code = GetterSetterUtil.getGetterStub(entry.field, entry.getIdentifier(), generateComments, modifier,
+						indent);
+			}
+
 			code = lineDelim + code;
 			String formatted = indentPattern(code, indent, lineDelim);
-			
+
 			if (i++ == 0) {
 				formatted = lineDelim + formatted;
 			}
-			
-			methods+= formatted;
 
-			
-		}						
-		
+			methods += formatted;
+
+		}
+
 		document.replace(end, 0, methods);
 		PDTFormatterUtils.format(document, new Region(end, methods.length()), type.getScriptProject().getProject());
 	}
-	
-	private String indentPattern(String originalPattern, String indentation,
-			String lineDelim) {
-		
+
+	private String indentPattern(String originalPattern, String indentation, String lineDelim) {
+
 		String delimPlusIndent = lineDelim + indentation;
-		String indentedPattern = originalPattern.replaceAll(lineDelim,delimPlusIndent) + delimPlusIndent;
+		String indentedPattern = originalPattern.replaceAll(lineDelim, delimPlusIndent) + delimPlusIndent;
 
 		return indentedPattern;
 	}
-	
-
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private void setupOptions() {
 
 		options = new HashMap(PHPCorePlugin.getOptions());
-		
-		IScopeContext[] contents = new IScopeContext[] {
-				new ProjectScope(type
-						.getScriptProject()
-						.getProject()),
-						InstanceScope.INSTANCE, DefaultScope.INSTANCE };
-		
+
+		IScopeContext[] contents = new IScopeContext[] { new ProjectScope(type.getScriptProject().getProject()),
+				InstanceScope.INSTANCE, DefaultScope.INSTANCE };
+
 		for (int i = 0; i < contents.length; i++) {
-			
+
 			IScopeContext scopeContext = contents[i];
 			IEclipsePreferences inode = scopeContext.getNode(PHPCorePlugin.ID);
-			
+
 			if (inode != null) {
-				
+
 				if (!options.containsKey(PHPCoreConstants.FORMATTER_USE_TABS)) {
-					
-					String useTabs = inode.get(PHPCoreConstants.FORMATTER_USE_TABS,null);
+
+					String useTabs = inode.get(PHPCoreConstants.FORMATTER_USE_TABS, null);
 					if (useTabs != null) {
 						options.put(PHPCoreConstants.FORMATTER_USE_TABS, useTabs);
 					}
 				}
-				
+
 				if (!options.containsKey(PHPCoreConstants.FORMATTER_INDENTATION_SIZE)) {
-					
-					String size = inode.get(PHPCoreConstants.FORMATTER_INDENTATION_SIZE,null);
-					
+
+					String size = inode.get(PHPCoreConstants.FORMATTER_INDENTATION_SIZE, null);
+
 					if (size != null) {
-						options.put(PHPCoreConstants.FORMATTER_INDENTATION_SIZE,size);
+						options.put(PHPCoreConstants.FORMATTER_INDENTATION_SIZE, size);
 					}
 				}
 			}
 		}
 	}
-	
-	
+
 	private void initialize(ExecutionEvent event, IModelElement element) throws InvalidElementException {
-		
+
 		editorPart = HandlerUtil.getActiveEditor(event);
 		textEditor = null;
 		if (editorPart instanceof PHPStructuredEditor)
@@ -414,138 +401,136 @@ public class GenerateGettersHandler extends SelectionHandler implements IHandler
 			if (o != null)
 				textEditor = (PHPStructuredEditor) o;
 		}
-		
+
 		document = textEditor.getDocument();
-		
+
 		setupOptions();
 
 	}
-	
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private Map getFields() throws ModelException {
-		
+
 		IField[] fields = type.getFields();
 		Map result = new LinkedHashMap();
 		for (int i = 0; i < fields.length; i++) {
-		    		    
+
 			IField field = fields[i];
-			
+
 			int flags = field.getFlags();
 			if (PHPFlags.isStatic(flags) || PHPFlags.isConstant(flags)) {
-			    continue;
+				continue;
 			}
-			
+
 			String name = field.getElementName().replace("$", "");
-            String getter = "get" + name.toLowerCase();
-            String setter = "set" + name.toLowerCase();
-			
+			String getter = "get" + name.toLowerCase();
+			String setter = "set" + name.toLowerCase();
+
 			List l = new ArrayList(2);
 			List<GetterSetterEntry> entries = new ArrayList<GetterSetterEntry>();
-			
+
 			boolean getterExists = false;
 			boolean setterExists = false;
-			
-            for (IMethod method : type.getMethods()) {
-                String methodName = method.getElementName().toLowerCase();
-                if (methodName.startsWith("get")) {
-                    getterExists = methodName.equals(getter);    
-                } else if (methodName.startsWith("set")) {
-                    setterExists = methodName.equals(setter);    
-                }
-            }
-			
-            if (!getterExists) {
-                entries.add(new GetterSetterEntry(field, true));
-            }
-            
-            if (!setterExists) {
-                entries.add(new GetterSetterEntry(field, false));
-            }
-            
-            if (!getterExists || !setterExists) {
-                l.addAll(entries);    
-            }
-            
+
+			for (IMethod method : type.getMethods()) {
+				String methodName = method.getElementName().toLowerCase();
+				if (methodName.startsWith("get")) {
+					getterExists = methodName.equals(getter);
+				} else if (methodName.startsWith("set")) {
+					setterExists = methodName.equals(setter);
+				}
+			}
+
+			if (!getterExists) {
+				entries.add(new GetterSetterEntry(field, true));
+			}
+
+			if (!setterExists) {
+				entries.add(new GetterSetterEntry(field, false));
+			}
+
+			if (!getterExists || !setterExists) {
+				l.addAll(entries);
+			}
+
 			if (!l.isEmpty())
 				result.put(field, l.toArray(new GetterSetterEntry[l.size()]));
 
 		}
 
 		return result;
-		
+
 	}
-	
+
 	private class InvalidElementException extends Exception {
 
 		private static final long serialVersionUID = 1L;
-		
+
 	}
-	
+
 	public static class GetterSetterEntry {
-		
-		
+
 		public final IField field;
 		public final boolean isGetter;
 		private String name = null;
 		private String identifier = null;
 		private String type = null;
 		private String raw = null;
-		
+
 		public GetterSetterEntry(IField field, boolean isGetterEntry) {
-		
+
 			this.field = field;
 			this.isGetter = isGetterEntry;
 		}
-		
+
 		public String getIdentifier() {
-			
+
 			if (identifier != null)
 				return identifier;
-			
+
 			if (isGetter)
 				return identifier = GetterSetterUtil.getGetterName(field);
-			else return identifier = GetterSetterUtil.getSetterName(field);
-			
+			else
+				return identifier = GetterSetterUtil.getSetterName(field);
+
 		}
-		
+
 		public String getType() {
 
 			if (type != null)
 				return type;
-			
+
 			return type = GetterSetterUtil.getTypeReference(field);
-			
+
 		}
-		
+
 		public String getName() {
 
 			if (name != null)
 				return name;
-			
+
 			if (isGetter) {
 				return name = String.format("%s()", getIdentifier());
 			} else {
-			    String elemName = field.getElementName();
-			    String type = getType();
-			    String param = "";
-			    if (type != null && type != "") {			       
-			        param += type +" ";
-			    }
-			    param += elemName;
+				String elemName = field.getElementName();
+				String type = getType();
+				String param = "";
+				if (type != null && type != "") {
+					param += type + " ";
+				}
+				param += elemName;
 				return name = String.format("%s(%s)", getIdentifier(), param);
 			}
 		}
-
 
 		public String getRawFieldName() {
 
 			if (raw != null)
 				return raw;
-			
+
 			return raw = field.getElementName().replace("$", "");
-			
+
 		}
-		
+
 	}
 }
--- org.pdtextensions.core/src/org/pdtextensions/core/util/PDTFormatterUtils.java
+++ org.pdtextensions.core/src/org/pdtextensions/core/util/PDTFormatterUtils.java
@@ -10,18 +10,29 @@ package org.pdtextensions.core.util;
 import java.util.HashMap;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.SafeRunner;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.link.LinkedModeModel;
+import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.DocumentChange;
+import org.eclipse.ltk.core.refactoring.IUndoManager;
+import org.eclipse.ltk.core.refactoring.RefactoringCore;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.php.internal.core.PHPVersion;
 import org.eclipse.php.internal.core.format.DefaultCodeFormattingProcessor;
 import org.eclipse.php.internal.core.format.ICodeFormattingProcessor;
 import org.eclipse.php.internal.core.format.IFormatterProcessorFactory;
 import org.eclipse.php.internal.core.project.ProjectOptions;
+import org.eclipse.php.internal.ui.PHPUiPlugin;
 import org.pdtextensions.core.log.Logger;
-import org.eclipse.php.internal.core.PHPVersion;
 
 @SuppressWarnings("restriction")
 public class PDTFormatterUtils {
@@ -30,7 +41,8 @@ public class PDTFormatterUtils {
 
 	private static IFormatterProcessorFactory getFormatterFactory() {
 		if (formatterFactory == null) {
-			IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(extensionName);
+			IConfigurationElement[] elements = Platform.getExtensionRegistry()
+					.getConfigurationElementsFor(extensionName);
 			for (int i = 0; i < elements.length; i++) {
 				final IConfigurationElement element = elements[i];
 				if (element.getName().equals("processor")) { //$NON-NLS-1$
@@ -53,22 +65,56 @@ public class PDTFormatterUtils {
 
 		return formatterFactory;
 	}
-	
-	public static ICodeFormattingProcessor createCodeFormatter(IDocument document, IRegion region, PHPVersion phpVersion, boolean useShortTags) throws Exception {
+
+	public static ICodeFormattingProcessor createCodeFormatter(IDocument document, IRegion region,
+			PHPVersion phpVersion, boolean useShortTags) throws Exception {
 		if (getFormatterFactory() != null) {
 			return formatterFactory.getCodeFormattingProcessor(document, phpVersion, useShortTags, region);
 		}
-		
+
 		return new DefaultCodeFormattingProcessor(new HashMap());
 	}
-	
+
 	public static void format(IDocument document, IRegion region, IProject project) {
+		DocumentChange documentChange = new DocumentChange("Format region", document);
 		try {
-			ICodeFormattingProcessor formatter = createCodeFormatter(document, region, ProjectOptions.getPhpVersion(project), ProjectOptions.useShortTags(project));
-			formatter.getTextEdits().apply(document);
-			
+			ICodeFormattingProcessor formatter = createCodeFormatter(document, region,
+					ProjectOptions.getPhpVersion(project), ProjectOptions.useShortTags(project));
+
+			documentChange.setEdit(formatter.getTextEdits());
+			if (document != null) {
+				LinkedModeModel.closeAllModels(document);
+			}
+
+			documentChange.initializeValidationData(new NullProgressMonitor());
+			RefactoringStatus valid = documentChange.isValid(new NullProgressMonitor());
+			if (valid.hasFatalError()) {
+				IStatus status = new Status(IStatus.ERROR, PHPUiPlugin.ID, IStatus.ERROR,
+						valid.getMessageMatchingSeverity(RefactoringStatus.FATAL), null);
+				throw new CoreException(status);
+			} else {
+				IUndoManager manager = RefactoringCore.getUndoManager();
+				Change undoChange;
+				boolean successful = false;
+				try {
+					manager.aboutToPerformChange(documentChange);
+					undoChange = documentChange.perform(new NullProgressMonitor());
+					successful = true;
+				} finally {
+					manager.changePerformed(documentChange, successful);
+				}
+				if (undoChange != null) {
+					undoChange.initializeValidationData(new NullProgressMonitor());
+					manager.addUndo("Undo formatting", undoChange);
+				}
+			}
+
 		} catch (Exception e) {
 			Logger.logException(e);
+		} finally {
+			if (documentChange != null) {
+				documentChange.dispose();
+			}
 		}
 	}
 }
